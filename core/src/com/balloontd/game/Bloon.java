2211bb90dda401835fa85344b666f5aabe387cde
chengyenkuo <b08902069@ntu.edu.tw>
2021-06-28 14:47:35 +0800

    .

--- core/src/com/balloontd/game/Bloon.java
+++ core/src/com/balloontd/game/Bloon.java
@@ -5,20 +5,42 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public abstract class Bloon extends Actor {
+    private boolean is_alive;
+    private Float distance, radius, speed;
+    
     @Override
-    public void act(float delta) {
-        // move bloon
+    public void act(float delta_t) {
+        this.distance += (delta_t*speed);
     }
 
     @Override
     abstract public void draw(Batch batch, float parentAlpha);
 
-    abstract public float getDistance();
-    abstract public void setDistance(float dist);
+    public float getDistance() {
+        return this.distance;
+    }
+
+    public void setDistance(float dist) {
+        this.distance = dist;
+    }
+
     abstract public Vector2 getCoords();
-    abstract public float getTiychRadius();
-    abstract public float getSpeed();
+
+    public float getTouchRadius() {
+        return this.radius;
+    }
+    
+    public float getSpeed() {
+        return this.speed;
+    }
+
     abstract public void pop(Dart dart);
-    abstract public void setAliveState(boolean is_alive);
-    abstract public boolean getAliveState();
+    
+    public void setAliveState(boolean is_alive) {
+        this.is_alive = is_alive;
+    }
+    
+    public boolean getAliveState() {
+        return this.is_alive;
+    };
 }
