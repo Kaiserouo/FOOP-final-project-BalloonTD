f23b4fd0139eeef2ec13461497c646e9a7561be6
chengyenkuo <b08902069@ntu.edu.tw>
2021-06-29 14:36:24 +0800

    more bloons

---
+++ core/src/com/balloontd/game/Bloons/BlackBloon.java
@@ -0,0 +1,41 @@
+package com.balloontd.game.bloons;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.balloontd.game.Bloon;
+import com.balloontd.game.Dart;
+import com.balloontd.game.GameScreen;
+
+import java.util.List;
+
+public class BlackBloon extends Bloon {
+    private static final TextureRegion region =
+            new TextureRegion(new Texture(Gdx.files.internal("black_bloon.png")));
+
+    public BlueBloon(GameScreen game_screen, Float distance) {
+        super(game_screen, region, distance);
+        this.speed = base_speed * 1F;
+        setTouchRadius(getTouchRadius() * 0.5F);
+        setScale(0.5F);
+    }
+
+    @Override
+    public void pop(Dart dart) {
+        game_screen.getPlayer().addMoney(1);
+        game_screen.getBloonManager().spawnBloons(this,
+                List.of(
+                    // list of new bloons
+                    // distance will be set in function so no worries
+                    new PinkBloon(game_screen, 0F),
+                    new PinkBloon(game_screen, 0F)
+                ), 15F
+        );
+        setAliveState(false);
+    }
+
+    @Override
+    public float costHealthAmt() {
+        return 6;
+    }
+}
---
+++ core/src/com/balloontd/game/Bloons/ZebraBloon.java
@@ -0,0 +1,41 @@
+package com.balloontd.game.bloons;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.balloontd.game.Bloon;
+import com.balloontd.game.Dart;
+import com.balloontd.game.GameScreen;
+
+import java.util.List;
+
+public class ZebraBloon extends Bloon {
+    private static final TextureRegion region =
+            new TextureRegion(new Texture(Gdx.files.internal("zebra_bloon.png")));
+
+    public BlueBloon(GameScreen game_screen, Float distance) {
+        super(game_screen, region, distance);
+        this.speed = base_speed * 1F;
+        setTouchRadius(getTouchRadius() * 0.5F);
+        setScale(0.5F);
+    }
+
+    @Override
+    public void pop(Dart dart) {
+        game_screen.getPlayer().addMoney(1);
+        game_screen.getBloonManager().spawnBloons(this,
+                List.of(
+                    // list of new bloons
+                    // distance will be set in function so no worries
+                    new BlackBloon(game_screen, 0F),
+                    new Whiteloon(game_screen, 0F)
+                ), 15F
+        );
+        setAliveState(false);
+    }
+
+    @Override
+    public float costHealthAmt() {
+        return 7;
+    }
+}
